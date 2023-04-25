07d90c2a70289694b8d266e5a82d7f824991de5d
Evgeny Gorelik <evgeny.gorelik@automotive-ai.com>
2023-04-25 16:14:11 +0200

    optimized hit-point calculation

--- logic/perception/hit_point_calculation.py
+++ logic/perception/hit_point_calculation.py
@@ -6,7 +6,7 @@ def snap_close_values(vec1, vec2, epsilon=1e-6):
     vec1[close_idx] = vec2[close_idx]
     return vec1 
 
-def calc_hit(ray: np.ndarray, convex_hull: ConvexHull):
+def calc_hit(ray: np.ndarray, convex_hull: ConvexHull, epsilon=1e-6):
     '''
         oriented and debugged implementation from reference: 
         https://stackoverflow.com/questions/30486312/intersection-of-nd-line-with-convex-hull-in-python
@@ -21,15 +21,17 @@ def calc_hit(ray: np.ndarray, convex_hull: ConvexHull):
                 np array of shape (dim, )
     '''
     eq = convex_hull.equations.T
-    A, b = eq[:-1].T, eq[-1]
+    A, b = eq[:-1].T, -eq[-1]
     ray /= np.linalg.norm(ray)
-    alpha = np.divide(-b, (A @ ray.reshape(-1,1)).squeeze())
+    alpha = np.divide(b, (A @ ray.reshape(-1,1)).squeeze())
+    left_side = (A @ np.outer(ray, alpha)).T
+    constrained = np.array([(left_side[i] - b <= epsilon).all() for i in range(len(alpha))])
     intersection_point = np.ones_like(ray) * np.inf
     intersection_point_norm = np.inf
-    for a in alpha:
+    for a in alpha[constrained]:
         intersection_candidate = ray * a
         intersection_candidate_norm = np.linalg.norm(intersection_candidate)
-        if (snap_close_values(A @ intersection_candidate.squeeze(),-b) <= -b).all() and intersection_candidate_norm < intersection_point_norm:
+        if intersection_candidate_norm < intersection_point_norm:
             intersection_point_norm = intersection_candidate_norm
             intersection_point = intersection_candidate
     return intersection_point
